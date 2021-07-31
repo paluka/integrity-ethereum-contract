915b5d0c5ab89657caa452f96e4542e2b7219095
Erik Paluka <epaluka@nuvalence.io>
2021-07-31 13:45:12 -0400

    Write Integrity smart contract

--- contracts/Integrity.sol
+++ contracts/Integrity.sol
@@ -2,6 +2,26 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Integrity {
-  constructor() public {
+  address public owner;
+  string[] data;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function upload(string calldata _text) restricted external {
+    data.push(_text);
+  }
+
+  function get() external view returns(string[] memory) {
+    return data;
+  }
+
+  modifier restricted() {
+    require(
+      msg.sender == owner,
+      "This function is restricted to the contract's owner"
+    );
+    _;
   }
 }
